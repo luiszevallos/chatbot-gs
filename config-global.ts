b8e891897ce2b8db5a56a11e4273df68cd4ad1ec
luis zevallos <luis_zevallos_17@hotmail.com>
2024-04-30 15:03:35 -0400

    web hook

--- config-global.ts
+++ config-global.ts
@@ -1,3 +1,4 @@
 export const HOST_API = process.env.HOST_API;
 export const HEADER_JWT = process.env.HEADER_JWT;
 export const NUMBER_PHONE_ID = process.env.NUMBER_PHONE_ID;
+export const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
--- src/controllers/webhook-controllers.ts
+++ src/controllers/webhook-controllers.ts
@@ -1,28 +1,36 @@
 import { Request, Response } from "express";
-import axios, { endpoints } from "../utils/axios";
+// import axios, { endpoints } from "../utils/axios";
+import { VERIFY_TOKEN } from "../../config-global";
 
 export const getWebhook = async (req: Request, res: Response) => {
-  const { body } = req;
-  console.log("🚀 ~ getWebhook ~ body:", body);
+  let mode = req.query["hub.mode"];
+  console.log("🚀 ~ getWebhook ~ mode:", mode);
+  let challenge = req.query["hub.challenge"];
+  console.log("🚀 ~ getWebhook ~ challenge:", challenge);
+  let token = req.query["hub.verify_token"];
+  console.log("🚀 ~ getWebhook ~ token:", token);
 
-  try {
-    const response = await axios.post(endpoints.messages, {
-      messaging_product: "whatsapp",
-      to: "584242755461",
-      type: "template",
-      template: {
-        name: "hello_world",
-        language: {
-          code: "en_US",
-        },
-      },
+  if (token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).json({
+      message: "Error en conectar bot",
     });
-    console.log("🚀 ~ getWebhook ~ response:", response);
-  } catch (error) {
-    console.log("🚀 ~ getWebhook ~ error:", error);
   }
-
-  res.status(200).json({
-    message: "Conectado a bot",
-  });
+  // try {
+  //   const response = await axios.post(endpoints.messages, {
+  //     messaging_product: "whatsapp",
+  //     to: "584242755461",
+  //     type: "template",
+  //     template: {
+  //       name: "hello_world",
+  //       language: {
+  //         code: "en_US",
+  //       },
+  //     },
+  //   });
+  //   console.log("🚀 ~ getWebhook ~ response:", response);
+  // } catch (error) {
+  //   console.log("🚀 ~ getWebhook ~ error:", error);
+  // }
 };
