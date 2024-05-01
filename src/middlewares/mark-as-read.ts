7c6824726981b32a957c560f6c9363e7746da3be
luis zevallos <luis_zevallos_17@hotmail.com>
2024-04-30 21:21:21 -0400

    post list 5

--- src/middlewares/mark-as-read.ts
+++ src/middlewares/mark-as-read.ts
@@ -1,18 +1,18 @@
 import { NextFunction, Response, Request } from "express";
-import axios, { endpoints } from "../utils/axios";
-import { IMessage } from "../types/webhook";
+// import axios, { endpoints } from "../utils/axios";
+// import { IMessage } from "../types/webhook";
 
 const markAsRead = async (req: Request, res: Response, next: NextFunction) => {
-  const { object, entry } = req.body;
+  // const { object, entry } = req.body;
 
-  if (object && entry) {
-    const messages = entry[0]?.changes[0]?.value?.messages || [];
-    await Promise.all(
-      messages.map(async (message: IMessage) => {
-        await axios.put(`${endpoints.messages}/${message.id}`);
-      })
-    );
-  }
+  // if (object && entry) {
+  //   const messages = entry[0]?.changes[0]?.value?.messages || [];
+  //   await Promise.all(
+  //     messages.map(async (message: IMessage) => {
+  //       await axios.put(`${endpoints.messages}/${message.id}`);
+  //     })
+  //   );
+  // }
 
   next();
 };
