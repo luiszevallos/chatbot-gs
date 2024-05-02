13e2ba16f99e32b4bce4b80f4e71f070ebf498ba
luis zevallos <luis_zevallos_17@hotmail.com>
2024-05-02 10:03:58 -0400

    post list 21

--- src/helpers/send-message-interactive.ts
+++ src/helpers/send-message-interactive.ts
@@ -9,7 +9,34 @@ const sendMessageInteractive = async (
     messaging_product: "whatsapp",
     type: "interactive",
     to,
-    interactive,
+    interactive: {
+      type: "list",
+      header: {
+        type: "text",
+        text: "<MESSAGE_HEADER_TEXT",
+      },
+      body: {
+        text: "<MESSAGE_BODY_TEXT>",
+      },
+      footer: {
+        text: "<MESSAGE_FOOTER_TEXT>",
+      },
+      action: {
+        sections: [
+          {
+            title: "<SECTION_TITLE_TEXT>",
+            rows: [
+              {
+                id: "<ROW_ID>",
+                title: "<ROW_TITLE_TEXT>",
+                description: "<ROW_DESCRIPTION_TEXT>",
+              },
+            ],
+          },
+        ],
+        button: "<BUTTON_TEXT>",
+      },
+    },
   });
 };
 
