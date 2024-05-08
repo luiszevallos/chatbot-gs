ddb5fc3342061cf64336e4941308e11e63cae3b8
luis zevallos <luis_zevallos_17@hotmail.com>
2024-05-08 10:12:43 -0400

    siclo 8.0.1

--- config-global.ts
+++ config-global.ts
@@ -1,5 +1,5 @@
-export const HOST_API = process.env.HOST_API;
-export const HEADER_JWT = process.env.HEADER_JWT;
-export const NUMBER_PHONE_ID = process.env.NUMBER_PHONE_ID;
+export const HOST_API = process.env.HOST_API_META;
+export const HEADER_JWT = process.env.ACCESS_TOKEN_META;
+export const NUMBER_PHONE_ID = process.env.NUMBER_PHONE_ID_META;
 export const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-export const HOST_API_SUPPORT = process.env.HOST_API_SUPPORT;
+export const HOST_API_SUPPORT = process.env.HOST_API;
